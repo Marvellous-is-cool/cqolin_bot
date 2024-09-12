25067ef7d2a45e781f047f48e7f9296bb7036b35
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2024-09-13 00:06:43 +0100

    mini modifications 6

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -10,7 +10,7 @@ exports.renderHomePage = (req, res) => {
 
   if (!username) {
     // If no username in session, render with empty links and handle in client
-    return res.render("home", { links: [], username: null });
+    return res.render("home", { links: [], categories: [], username: null });
   }
 
   firebaseController
@@ -18,15 +18,19 @@ exports.renderHomePage = (req, res) => {
     .then((firebaseLinks) => {
       // If links found in Firebase, render them
       if (firebaseLinks && firebaseLinks.length > 0) {
-        res.render("home", { links: firebaseLinks, username });
+        res.render("home", {
+          links: firebaseLinks,
+          categories: categories,
+          username,
+        });
       } else {
         // Render with empty links if none found in Firebase
-        res.render("home", { links: [], username });
+        res.render("home", { links: [], categories: [], username });
       }
     })
     .catch((error) => {
       console.log("Error fetching links from Firebase: ", error);
       // In case of an error, render with empty links and handle in client
-      res.render("home", { links: [], username });
+      res.render("home", { links: [], categories: [], username });
     });
 };
